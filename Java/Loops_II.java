90cc74740d3db7430bfab3d76e6bd660868ce15d
Derkopath <derkius1@gmail.com>
2024-07-17 14:21:21 +0200

    First exercise changes.

--- Java/Loops_II.java
+++ Java/Loops_II.java
@@ -4,28 +4,21 @@ import java.util.*;
 
 class Loops_II{
     public static void main(String []argh){
-        /*
         Scanner in = new Scanner(System.in);
-        System.out.print("Numero de consultas: ");
         int t=in.nextInt();
-        in.close();
-        */
-
-        int t = 1;
-
         for(int i=0;i<t;i++){
-            int a = 3;
-            int b = 3;
-            int n = 3;
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
 
-            for(int j=0;j<n;j++) {
-
+            for(int j=0; j<n; j++){
                 a = a + (int)Math.pow(2,j) * b;
 
                 System.out.print(a + " ");
             }
             System.out.println("");
-        }
 
+        }
+        in.close();
     }
 }
